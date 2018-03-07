a2786fef1432fec126bfcaa459dd64705870157d
iago <iago.garcia.carpintero@gmail.com>
2018-03-07 17:57:16 +0100

    C1 - se corrige bug de Eclipse con array

--- 08_Repaso_Ej4/src/uno/Comanda.java
+++ 08_Repaso_Ej4/src/uno/Comanda.java
@@ -3,8 +3,7 @@ package uno;
 public class Comanda {
     
     private Integer numComensales;
-    private int pedidos[][];
     
-    pedidos = new Integer[2][3];
-
+    private Integer[] numdos;
+    
 }
